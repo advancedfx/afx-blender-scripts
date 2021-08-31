bd9291ed606ea2b8176fc58e9ac57d7f40c9771c
Devo <bnn1@hotmail.de>
2021-08-31 17:17:26 +0200

    v1.12.6 (2021-08-31T17:17Z)
    - added skip import option for Stattrack and Stickers
    - added skip import for shared_player_skeleton to Skip Physic and LOD Meshes

--- advancedfx/__init__.py
+++ advancedfx/__init__.py
@@ -3,7 +3,7 @@ import bpy
 bl_info = {
 	"name": "advancedfx Blender Scripts",
 	"author": "advancedfx.org",
-	"version": (1, 12, 5),
+	"version": (1, 12, 6),
 	"blender": (2, 80, 0),
 	"location": "File > Import/Export",
 	"description": "For inter-operation with HLAE.",
